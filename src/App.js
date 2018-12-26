acbf454a2d32dd27248998dfe0d536ee9720c7d2
Buck Pucker <egd1753@gmail.com>
2018-12-26 15:19:42 -0600

    did some work on checkpoint 2 roomlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAuHGz9PM2QsFhtOKf0omEXP7VT_6jXlfs",
+  authDomain: "bloc-chat-react-c9b16.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-c9b16.firebaseio.com",
+  projectId: "bloc-chat-react-c9b16",
+  storageBucket: "bloc-chat-react-c9b16.appspot.com",
+  messagingSenderId: "762262567180"
+};
+
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <main>
+        <RoomList fireBase={firebase}/>
+        {/*
+        <header>
+          <nav>
+            <Link to='/'>RoomList</Link>
+          </nav>
+          <h1>Bloc Chat</h1>
         </header>
+        <main>
+          <Route exact path="/" component={RoomList} />
+        </main>
+        */}
+        </main>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            rooms: [],
+        };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms'); 
+    }
+
+    
+    componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat( room ) })
+        });
+    }
+
+ 
+    render() {
+        return (
+            <section className='RoomListComponent'>
+                <div className='LColumn-RoomList'>
+                    <header>
+                        <h2>Bloc Chat</h2>
+                    </header>
+                    <nav>
+                        <ul>
+                            {
+                                this.state.rooms.map( (room, index) => 
+                                    <li
+                                        key={index}
+                                        className='room'
+                                    >
+                                    {rooms.name}
+                                    </li>
+                                )
+                            }
+                        </ul> 
+                    </nav> 
+                </div>
+                <div className='RColum-MessageArea'>
+                    <header className='MessageHeader'>
+                        <h1></h1>
+                    </header>
+                    <main className='Messages'>
+
+                    </main>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default RoomList;
